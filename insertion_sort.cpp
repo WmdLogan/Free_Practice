ab2895ef706c8893f7ad9f992e090c772f936b72
WmdLogan <710222440@qq.com>
2020-03-23 00:05:37 +0800

    first commit

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -4,7 +4,7 @@
 #include "iostream"
 void insertion_sort(int a[], int n){
     int i, j, v;
-    for(i = 1;i < n; i++){
+    for (i = 1; i < n; i++) {
         for (j = i - 1, v = a[i]; j >= 0 && a[j] > v; j--) {
             a[j + 1] = a[j];
         }
